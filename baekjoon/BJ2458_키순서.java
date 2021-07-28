c6ae38cd2c7bf1ed75b138a4276afebcfbac4a1c
gyulgyul912 <77738908+gyulgyul912@users.noreply.github.com>
2021-07-28 23:54:59 +0900

    Add files via upload

--- baekjoon/BJ2458_키순서.java
+++ baekjoon/BJ2458_키순서.java
@@ -8,52 +8,52 @@ import java.util.StringTokenizer;
 
 public class BJ2458_키순서 {
 	
-	static int[][] students;
+	
 	static int INF = 1000;
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		students = new int[N][N];
-		for(int i=0;i<N;i++) {
-			Arrays.fill(students[i], INF);
-			students[i][i] = 0;
-		}
+		int[][] students = new int[N+1][N+1];
+		
+//		for(int i=1;i<=N;i++) {
+//			Arrays.fill(students[i], INF);
+//			students[i][i] = 0;
+//		}
 		
 		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(in.readLine());
-			int from = Integer.parseInt(st.nextToken())-1;
-			int to = Integer.parseInt(st.nextToken())-1;			
-			students[from][to] = 1;
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			students[a][b] = 1;	
 		}
-		// FloydWarshall
-		for(int k=0;k<N;k++) { // 경유지
-			for(int i=0;i<N;i++) { // 출발점
-				for(int j=0;j<N;j++) { // 도착점
-					if(i==j ||k==i ||k==j)continue;
-					if(students[i][j]>students[i][k]+students[k][j]) {
-						students[i][j] = students[i][k]+students[k][j];
 
+		for(int k=1;k<=N;k++) {
+			for(int i=1;i<=N;i++) {
+				for(int j=1;j<=N;j++) {
+					if(k==i || k==j ||i==j)continue;
+//					students[i][j]>students[i][k]+studetns[k][j]
+					if(students[i][k]==1 &&students[k][j]==1) {
+						students[i][j] = 1;
 					}
 				}
 			}
 		}
-		// 키 몇등인지 확인
-		int answer = 0;
-		for(int i=0;i<N;i++) {
+		int ans = 0;
+		for(int i=1;i<=N;i++) {
 			int cnt = 0;
-			for(int j=0;j<N;j++) {
-				if(i==j)continue;
-				if(students[i][j]<INF ||students[j][i]<INF)cnt++;
+			for(int j=1;j<=N;j++) {
+				if(i==j)continue; 
+				if(students[i][j]==1 || students[j][i]==1)cnt++;
 			}
-			if(cnt==N-1)answer++;
+			if(cnt==N-1)ans++;
 		}
-		System.out.println(answer);
+		System.out.println(ans);
+		
 	}
 
 }
